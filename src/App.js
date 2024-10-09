ee800d4c82e413d34e4a4f9ed0717a2c50e3e98b
Solomon Thomas <sthoma25@nd.edu>
2024-10-09 19:18:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import parse from 'parse';
+import * as ENV from "./environments.js"
+
+// initializing 
+parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY,);
+parse.serverURL(ENV.SERVER_URL);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: "MdlvimNvxiv0ydbXH10MZVtx94b0kNfHAUkug4nZ",
+    JAVASCRIPT_KEY: "ppbj5Im1nEkPyQrn0Ui3agREMjXemx7dYBukxSAw",
+    SERVER_URL: "https:/parseapi.back4app.com"
+}
