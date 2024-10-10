a72dab68f851f847cc91bc6bc6efdfdcca7fdb02
Solomon Thomas <sthoma25@nd.edu>
2024-10-09 20:22:24 -0400

    kickoff

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
 // Parse.serverURL(ENV.SERVER_URL);
 Parse.serverURL = ENV.SERVER_URL;
 
+// this is my try for the feature 4 kickoff
+const Kickoff = Parse.Object.extend("Kickoff");
+const kickoff = new Kickoff();
+
+kickoff.set("attempt", 4444);
+
+kickoff.save()
+.then((kickoff) => {
+  // Execute any logic that should take place after the object is saved.
+  alert('New object created with objectId: ' + kickoff.id);
+}, (error) => {
+  // Execute any logic that should take place if the save fails.
+  // error is a Parse.Error with an error code and message.
+  alert('Failed to create new object, with error code: ' + error.message);
+});
+
+
 function App() {
   return (
     <div className="App">
